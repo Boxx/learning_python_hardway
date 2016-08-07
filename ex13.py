d588758a0386a3707e0e6637131c604eab9b29d7
Brandon Nelson <brandon.lnelson@gmail.com>
2016-08-06 18:49:32 -0700

    Exercises 13-20

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+# ARGS 0,1,2,3
+# arg 0 is the script being run
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
